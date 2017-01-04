848db819f1b213b27ea211d8b41f35764e2b85c6
Rodrigo Dumont <rlsdumont@gmail.com>
2017-01-04 12:06:43 -0200

    Create metadata forwarder in request context

--- clients/requestContext.go
+++ clients/requestContext.go
@@ -5,48 +5,65 @@ import (
 	"sync"
 )
 
-type RequestContext interface {
+const (
+	metadataHeader = "X-Vtex-Meta"
+)
+
+type HeaderTracker interface {
 	Parse(h http.Header)
 	Write(w http.ResponseWriter)
+}
+
+type RequestContext interface {
+	HeaderTracker
 	getCache() *CacheConfig
 }
 
 func NewRequestContext(cache *CacheConfig) RequestContext {
 	return &requestContext{
-		trackedKeys: []string{"X-Vtex-Meta"},
-		headers:     http.Header{},
-		cache:       cache,
+		trackers: []HeaderTracker{newMetadataForwarder()},
+		cache:    cache,
 	}
 }
 
 type requestContext struct {
-	trackedKeys []string
-	headers     http.Header
-	lock        sync.RWMutex
-
-	cache *CacheConfig
+	trackers []HeaderTracker
+	cache    *CacheConfig
 }
 
 func (c *requestContext) Parse(h http.Header) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
-
-	for _, k := range c.trackedKeys {
-		for _, v := range h[k] {
-			c.headers.Add(k, v)
-		}
+	for _, t := range c.trackers {
+		t.Parse(h)
 	}
 }
 
 func (c *requestContext) Write(w http.ResponseWriter) {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
-	h := w.Header()
-	for k, v := range c.headers {
-		h[k] = v
+	for _, t := range c.trackers {
+		t.Write(w)
 	}
 }
 
 func (c *requestContext) getCache() *CacheConfig {
 	return c.cache
+}
+
+type metadataForwarder struct {
+	metadata []string
+	lock     sync.RWMutex
+}
+
+func newMetadataForwarder() *metadataForwarder {
+	return &metadataForwarder{[]string{}, sync.RWMutex{}}
+}
+
+func (f *metadataForwarder) Parse(h http.Header) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+	f.metadata = append(f.metadata, h[metadataHeader]...)
+}
+
+func (f *metadataForwarder) Write(w http.ResponseWriter) {
+	f.lock.RLock()
+	defer f.lock.RUnlock()
+	w.Header()[metadataHeader] = f.metadata
 }
