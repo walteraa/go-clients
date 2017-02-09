6f5af10288e26ae1d6c3393593b28b0166c81dfa
Gabriel Lima <gabriel.lima@vtex.com.br>
2017-02-09 19:35:07 -0200

    Adds logging functionalities to colossus client

--- colossus/client.go
+++ colossus/client.go
@@ -11,6 +11,8 @@ import (
 type Colossus interface {
 	SendEventJ(account, workspace, sender, subject, key string, body interface{}) error
 	SendEventB(account, workspace, sender, subject, key string, body []byte) error
+	SendLogJ(account, workspace, sender, subject, level string, body interface{}) error
+	SendLogB(account, workspace, sender, subject, level string, body []byte) error
 }
 
 type Client struct {
@@ -24,6 +26,7 @@ func NewClient(endpoint, authToken, userAgent string, reqCtx clients.RequestCont
 
 const (
 	eventPath = "/%v/%v/events/%v/%v/%v"
+	logPath   = "/%v/%v/logs/%v/%v/%v"
 )
 
 func (cl *Client) SendEventJ(account, workspace, sender, subject, key string, body interface{}) error {
@@ -37,6 +40,22 @@ func (cl *Client) SendEventJ(account, workspace, sender, subject, key string, bo
 func (cl *Client) SendEventB(account, workspace, sender, subject, key string, body []byte) error {
 	_, err := cl.http.Post().
 		AddPath(fmt.Sprintf(eventPath, account, workspace, sender, subject, key)).
+		Body(bytes.NewReader(body)).Send()
+
+	return err
+}
+
+func (cl *Client) SendLogJ(account, workspace, sender, subject, level string, body interface{}) error {
+	_, err := cl.http.Post().
+		AddPath(fmt.Sprintf(logPath, account, workspace, sender, subject, level)).
+		JSON(body).Send()
+
+	return err
+}
+
+func (cl *Client) SendLogB(account, workspace, sender, subject, level string, body []byte) error {
+	_, err := cl.http.Post().
+		AddPath(fmt.Sprintf(logPath, account, workspace, sender, subject, level)).
 		Body(bytes.NewReader(body)).Send()
 
 	return err
